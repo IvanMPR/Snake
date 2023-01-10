2af5386bbedc93862a4830bbda8c75ba1e7a02c7
Ivan MPR <ivsanmas@gmail.com>
2023-01-10 22:34:01 +0100

    dev

--- script.js
+++ script.js
@@ -8,37 +8,51 @@ const helper = {
   position: { x: 0, y: 0 },
   points: 0,
   moves: [
-    { x: 1, y: Math.floor(BOARD_LENGTH / 2) },
-    { x: 2, y: Math.floor(BOARD_LENGTH / 2) },
+    { x: 1, y: 7 },
+    { x: 2, y: 7 },
+    { x: 3, y: 7 },
+    { x: 4, y: 7 },
   ],
   // isGameOver: true,
   level: 2,
   timeSinceLastRender: 0,
+  currentDir: null,
 };
 
-const nextField = () => {
-  const currentSnakeHead = helper.moves.slice(-1);
-  const nextSnakeHead = currentSnakeHead.map(position => {
-    position.x + helper.position.x;
-    position.y + helper.position.y;
-    console.log(position);
-  });
-  console.log(...nextSnakeHead);
-  helper.moves.push(...nextSnakeHead);
-  helper.moves.shift();
-  helper.position.x = 0;
-  helper.position.y = 0;
-};
-
-function updateSnake() {
-  console.log('update');
-  nextField();
+function updateSnake(event) {
+  // copy last snake
+  let copy = helper.moves.slice();
+  // clear old snake
+  helper.moves = [];
+  // create new snake
+  //  all fields except first and last
+  const snakeBody = copy.slice(1, -1);
+  // last field
+  const snakeHead = copy.slice(-1);
+  console.log(snakeHead);
+  let position = { x: snakeHead[0].x, y: snakeHead[0].y };
+  const nextSnakeHeadCoords = e => {
+    return (e.key = 'ArrowUp'
+      ? (position = { x: snakeHead[0].x, y: snakeHead[0].y - 1 })
+      : (e.key = 'ArrowDown'
+          ? (position = { x: snakeHead[0].x, y: snakeHead[0].y + 1 })
+          : (e.key = 'ArrowLeft'
+              ? (position = { x: snakeHead[0].x - 1, y: snakeHead[0].y })
+              : (position = { x: snakeHead[0].x + 1, y: snakeHead[0].y }))));
+  };
+  nextSnakeHeadCoords(event);
+  helper.moves.push(...snakeBody);
+  helper.moves.push(position);
+  // console.log(position, 'POS');
+  // console.log(snakeBody, 'snake body', last, 'last');
+  // console.log('update');
+  // console.log(...last);
 }
 
 function renderSnake() {
-  Array.from(document.querySelectorAll('.snake')).forEach(div => {
-    div.parentElement.removeChild(div);
-  });
+  // Array.from(document.querySelectorAll('.snake')).forEach(div => {
+  //   div.parentElement.removeChild(div);
+  // });
   helper.moves.forEach(element => {
     const snakePart = document.createElement('div');
     snakePart.style.gridRowStart = element.y;
@@ -46,8 +60,9 @@ function renderSnake() {
     snakePart.classList.add('snake');
     grid.appendChild(snakePart);
   });
+  console.log('render');
 }
-
+renderSnake();
 function mainGameLoop(timeStamp) {
   requestAnimationFrame(mainGameLoop);
   const secondsSinceLastRender =
@@ -67,7 +82,6 @@ addEventListener('keydown', moveDown);
 function moveDown(e) {
   if (e.key !== 'ArrowDown' || helper.currentDir === 'up') return;
   helper.position.y += 1;
-  nextField();
 }
 
 button.addEventListener('click', () => {
