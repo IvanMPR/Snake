4784d1f658118ce0b09b4193b3cd3c3820d0e694
Ivan MPR <ivsanmas@gmail.com>
2023-01-20 12:13:43 +0100

    dev

--- script.js
+++ script.js
@@ -20,17 +20,16 @@ function updateSnake() {
     x: snakeHead.x + helper.currentPoss.x,
     y: snakeHead.y + helper.currentPoss.y,
   };
+  console.log(newHead, 'newHead');
   if (newHead.x < 1 || newHead.x > 15 || newHead.y < 1 || newHead.y > 15) {
     helper.isGameOver = true;
     console.log('game over');
+    return;
   }
 
   helper.moves.shift();
   helper.moves.push(newHead);
-  console.log('update');
-  // helper.position.x = 0;
-  // helper.position.y = 0;
-  // console.log(helper.x, helper.y);
+  // console.log('update');
 }
 
 function renderSnake() {
@@ -48,8 +47,9 @@ function renderSnake() {
 renderSnake();
 function mainGameLoop(timeStamp) {
   if (helper.isGameOver) return;
+
   requestAnimationFrame(mainGameLoop);
-  const secondsSinceLastRender = (timeStamp - helper.timeSinceLastRender) / 750;
+  const secondsSinceLastRender = (timeStamp - helper.timeSinceLastRender) / 400;
   if (secondsSinceLastRender < 1 / helper.level) return;
   helper.timeSinceLastRender = timeStamp;
   // console.log(timeStamp);
@@ -57,7 +57,13 @@ function mainGameLoop(timeStamp) {
   updateSnake();
   renderSnake();
 }
-
+// helper function that calls update, render and requestAnimationFrame.
+// reduces lines of code to be called in moveUp, moveDown, moveLeft and moveRight functions
+function nextFrame() {
+  updateSnake();
+  renderSnake();
+  requestAnimationFrame(mainGameLoop);
+}
 function moveUp(e) {
   console.log(e.key);
   if (e.key !== 'ArrowUp') return;
@@ -67,25 +73,39 @@ function moveUp(e) {
   }
   if (!helper.isGameOver && helper.currentPoss.y === 1) return;
   helper.currentPoss = { x: 0, y: -1 };
-  // if (e.key === 'ArrowUp' && helper.isGameOver) {
-  //   helper.isGameOver = false;
-  //   requestAnimationFrame(mainGameLoop);
-  // } else {
-  //   if (helper.currentPoss.y === 1) return;
-  //   else helper.currentPoss = { x: 0, y: -1 };
-  // }
+  nextFrame();
 }
+
 function moveDown(e) {
+  if (e.key !== 'ArrowDown') return;
+  if (helper.isGameOver) {
+    helper.isGameOver = false;
+    requestAnimationFrame(mainGameLoop);
+  }
   if (e.key !== 'ArrowDown' || helper.currentPoss.y === -1) return;
   helper.currentPoss = { x: 0, y: 1 };
+  nextFrame();
 }
+
 function moveLeft(e) {
+  if (e.key !== 'ArrowLeft') return;
+  if (helper.isGameOver) {
+    helper.isGameOver = false;
+    requestAnimationFrame(mainGameLoop);
+  }
   if (e.key !== 'ArrowLeft' || helper.currentPoss.x === 1) return;
   helper.currentPoss = { x: -1, y: 0 };
+  nextFrame();
 }
 function moveRight(e) {
+  if (e.key !== 'ArrowRight') return;
+  if (helper.isGameOver) {
+    helper.isGameOver = false;
+    requestAnimationFrame(mainGameLoop);
+  }
   if (e.key !== 'ArrowRight' || helper.currentPoss.x === -1) return;
   helper.currentPoss = { x: 1, y: 0 };
+  nextFrame();
 }
 
 // button.addEventListener('click', () => {
